4cde05a2678c85d7e088f2f59ed8f2ac888ecaef
Ellipse <ke.qiu@zju.edu.cn>
2023-01-26 18:10:37 +0800

    Qiu Ke urdf motion

--- ctm_mpt_kernal/src/vrt_cntlr_main.cpp
+++ ctm_mpt_kernal/src/vrt_cntlr_main.cpp
@@ -4,6 +4,14 @@
 #include <string>
 #include <fstream>
 
+#include <nav_msgs/Path.h>
+#include <std_msgs/String.h>
+#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+
 void proc(size_t cur, size_t tot);
 
 int main(int argc, char** argv)
@@ -36,9 +44,17 @@ int main(int argc, char** argv)
 	joint_state.name[16]="S8J9";
 	joint_state.name[17]="J9S9";
 
-	ros::Rate loop_rate(10);
-	ros::Duration(4).sleep(); // Wait for RViz to open.
-	while (ros::ok())
+	tf::TransformListener tf_listener;
+	ros::Publisher path_pub = nh.advertise<nav_msgs::Path>("history_path", 1000);
+	ros::Time t_cur;
+    t_cur = ros::Time::now();
+    nav_msgs::Path path;
+    path.header.stamp = t_cur;
+    path.header.frame_id = "Ref";
+
+	ros::Rate loop_rate(100);
+	ros::Duration(10).sleep(); // Wait for RViz to open.
+	if (ros::ok())
 	{
 		std::string fn("/home/ellipse/Desktop/catkin_ws/src/ctm_mpt/ctm_mpt_kernal/src/fq.txt");
 		std::ifstream fin(fn);
@@ -71,8 +87,36 @@ int main(int argc, char** argv)
 			jnt_pub.publish(joint_state);
 			cur += 1;
 			proc(cur, tot);
-			std::getchar(); // For debug only.
-			// loop_rate.sleep();
+			// std::getchar(); // For debug only.
+			loop_rate.sleep();
+
+			// Publish history path.
+			tf::StampedTransform transform;
+			try
+			{
+				tf_listener.lookupTransform("/Ref", "/Tip", ros::Time(0), transform);
+			}
+			catch (tf::TransformException ex)
+			{
+				ROS_ERROR("%s",ex.what());
+				ros::Duration(0.5).sleep();
+			}
+
+			t_cur = ros::Time::now();
+
+			geometry_msgs::PoseStamped pose_stamped;
+			pose_stamped.pose.position.x = transform.getOrigin().x();
+			pose_stamped.pose.position.y = transform.getOrigin().y();
+			pose_stamped.pose.position.z = transform.getOrigin().z();
+			pose_stamped.pose.orientation.x = transform.getRotation().x();
+			pose_stamped.pose.orientation.y = transform.getRotation().y();
+			pose_stamped.pose.orientation.z = transform.getRotation().z();
+			pose_stamped.pose.orientation.w = transform.getRotation().w();
+
+			path.header.stamp = t_cur;
+			path.poses.push_back(pose_stamped);
+
+			path_pub.publish(path);
 		}
 		fin.clear();
 		fin.close();
